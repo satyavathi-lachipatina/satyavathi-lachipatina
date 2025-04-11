724c3bf81042fee3933053aa4b9fcdaf73de6897
satyavathi-lachipatina <hemalathalatchipathini@gmail.com>
2025-04-11 23:02:44 +0530

    Create Inheritance.java

---
+++ Inheritance.java
@@ -0,0 +1,18 @@
+
+class Animal {
+    void eat() {
+        System.out.println("Eating...");
+    }
+}
+class Dog extends Animal {
+    void bark() {
+        System.out.println("Barking...");
+    }
+}
+public class Inheritance {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat(); // Inherited method
+        dog.bark(); // Dog's own method
+    }
+}
